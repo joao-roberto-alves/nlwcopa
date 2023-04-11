5b7d329aa71868c6d4317b9b9794208f935ae856
João Roberto~ <joaoroberto1406@outlook.com>
2023-04-11 16:11:32 -0300

    fix error

--- main.js
+++ main.js
@@ -1,44 +1,52 @@
 function createGame(player1, hour, player2) {
-    return `
+  return `
         <li>
             <img src="./assets/icon-${player1}.svg" alt="bandeira do ${player1}">
             <strong>${hour}</strong>
             <img src="./assets/icon-${player2}.svg" alt="bandeira da ${player2}">
         </li>
-    ` 
+    `
 }
 
-let delay = -0.4;
+let delay = -0.4
 
-function createCard(date, day , games) {
-    delay = delay + 0.4; 
-    return `
+function createCard(date, day, games) {
+  delay = delay + 0.4
+  return `
         <div class="card" style="animation-delay: ${delay}s">
                 <h2> ${date} <span>${day}</span></h2>
                 <ul>                
                     ${games}
                 </ul>
         </div>
-    `    
+    `
 }
-document.querySelector("#cards").innerHTML = 
-    
-        
-      createCard('24/11', 'quinta', 
-        createGame('brazil','09:00', 'serbia') + createGame('korea','12:00', 'spain') + createGame('ghana','16:00', 'arg') )
-        +
-
-
-       createCard('28/11', 'segunda', 
-        createGame('suice','09:00', 'brazil') + createGame('portugal','12:00', 'korea') + createGame('portugal','16:00', 'spain') )  
-        +
-
-        createCard('02/12', 'sexta',
-        createGame('brazil','09:00', 'cameron') + createGame('korea','12:00', 'uruguai') + createGame('spain','16:00', 'arg')) 
-        +
-        
-        createCard('28/11', 'segunda', 
-        createGame('suice','09:00', 'brazil') + createGame('arg','12:00', 'uruguai') + createGame('portugal','16:00', 'france') )  
-      
-
-
+document.querySelector("#cards").innerHTML =
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("brazil", "09:00", "serbia") +
+      createGame("korea", "12:00", "spain") +
+      createGame("ghana", "16:00", "arg")
+  ) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("suice", "09:00", "brazil") +
+      createGame("portugal", "12:00", "korea") +
+      createGame("portugal", "16:00", "spain")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("brazil", "09:00", "cameron") +
+      createGame("korea", "12:00", "uruguai") +
+      createGame("spain", "16:00", "arg")
+  ) +
+  createCard(
+    "06/12",
+    "terça",
+    createGame("arg", "09:00", "france") +
+      createGame("brazil", "12:00", "uruguai") +
+      createGame("portugal", "16:00", "korea")
+  )
